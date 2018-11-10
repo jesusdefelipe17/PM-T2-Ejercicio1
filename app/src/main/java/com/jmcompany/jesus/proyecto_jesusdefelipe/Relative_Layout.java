6c313712faa335f3adb22f8d43e94e3da2d0e30d
unknown <jesusdefelipe17@gmail.com>
2018-11-10 12:26:15 +0100

    tercer commit

--- app/src/main/java/com/jmcompany/jesus/proyecto_jesusdefelipe/Relative_Layout.java
+++ app/src/main/java/com/jmcompany/jesus/proyecto_jesusdefelipe/Relative_Layout.java
@@ -1,20 +1,19 @@
 package com.jmcompany.jesus.proyecto_jesusdefelipe;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Linear_layout extends AppCompatActivity {
+public class Relative_Layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_layout);
+        setContentView(R.layout.activity_relative__layout);
     }
-    public void cambiarActivity(View view) {
 
-        Intent intent = new Intent(this,Relative_Layout.class);
-        startActivity(intent);
+    public void norte(View view) {
+
+
     }
 }
